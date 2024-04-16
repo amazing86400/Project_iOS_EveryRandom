8234c44a6e41ed031e407e4208238898d150ce4e
kbshin <amazing1997@naver.com>
2024-04-16 20:42:12 +0900

    Initial Commit

---
+++ EveryRandom/EveryRandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryRandomApp.swift
+//  EveryRandom
+//
+//  Created by KIBEOM SHIN on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct EveryRandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
