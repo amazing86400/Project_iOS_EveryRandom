65483ef9258360aa4381b8de0fa33b1db2438b48
kbshin <amazing1997@naver.com>
2024-04-16 21:29:40 +0900

    1차 작업 완료

--- EveryRandom/ContentView.swift
+++ EveryRandom/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: 변수 초기화
+    @State private var names: [String] = []
+    @State private var inputedName = ""
+    @State private var showAlert = false
+    @State private var showAlert2 = false
+    @State private var result = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // 상단 로고
+            VStack {
+                Image(systemName: "gamecontroller.fill")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.tint)
+                    .padding(10)
+                Text("모두의 랜덤 게임")
+                    .font(.title2)
+                    .bold()
+            }
+            .padding()
+            
+            // 리스트
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+                .onDelete(perform: delete)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            // 이름 입력
+            TextField("이름을 입력해 주세요.", text: $inputedName)
+                .onSubmit {
+                    if !inputedName.isEmpty && !names.contains(inputedName) {
+                        names.append(inputedName)
+                        inputedName = ""
+                    } else {
+                        showAlert = true
+                    }
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("경고"),
+                        message: Text(inputedName.isEmpty ? "이름을 입력하세요." : names.contains(inputedName) ? "이미 똑같은 이름이 있어요." : "이름을 다시 입력해 주세요."),
+                        dismissButton: .default(Text("확인"))
+                    )
+                }
+                .padding(.bottom, 30)
+            
+            // 뽑기 버튼
+            Button("랜덤 뽑기!!") {
+                if let randomValue = names.randomElement() {
+                    result = randomValue
+                    showAlert2 = true
+                    names = []
+                }
+            }
+            .alert(isPresented: $showAlert2) {
+                Alert(title: Text("ㅋㅋㅋㅋㅋ"),
+                      message: Text(result),
+                      dismissButton: .default(Text("확인")))
+            }
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue, in: RoundedRectangle(cornerRadius: 10))
         }
         .padding()
+    }
+    
+    // MARK: List 이름 제거 함수
+    func delete(at offsets: IndexSet) {
+        names.remove(atOffsets: offsets)
     }
 }
 
