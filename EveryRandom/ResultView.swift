0ac5e7c66d4900ceefdf81ed7176701eeff44c56
kbshin <amazing1997@naver.com>
2024-04-18 21:27:08 +0900

    4차 작업 완료

---
+++ EveryRandom/ResultView.swift
@@ -0,0 +1,56 @@
+//
+//  ResultView.swift
+//  EveryRandom
+//
+//  Created by KIBEOM SHIN on 4/18/24.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    @Binding var showResultView: Bool
+    @Binding var animationCount: Int
+    let winner: String
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            Text("축하합니다")
+                .font(.headline)
+            
+            HStack(alignment: .center, spacing: 10) {
+                Spacer()
+                Image(systemName: "heart.fill")
+                    .symbolEffect(.bounce.up.wholeSymbol, options: .repeat(animationCount), value: 2)
+                    .foregroundStyle(.pink)
+                Text(winner)
+                    .font(.title)
+                    .bold()
+                Image(systemName: "heart.fill")
+                    .symbolEffect(.bounce.up.wholeSymbol, options: .repeat(animationCount), value: 2)
+                    .foregroundStyle(.pink)
+                Spacer()
+            }
+            .font(.title2)
+            .bold()
+            .padding()
+            
+            Button {
+                showResultView = false
+            } label: {
+                Text("확인")
+                    .padding(.horizontal, 25)
+                    .frame(height: 35)
+                    .foregroundColor(.white)
+                    .bold()
+                    .background(.pink)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+        .background(.white, in: RoundedRectangle(cornerRadius: 20))
+    }
+}
+
+#Preview {
+    ResultView(showResultView: .constant(true), animationCount: .constant(2), winner: "신기범")
+}
